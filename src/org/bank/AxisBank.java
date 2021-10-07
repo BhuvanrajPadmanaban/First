c83dbffea1028c2abb45886ceb4c17e78014ba12
BhuvanrajPadmanaban <bhuvan0096@gmail.com>
2021-10-07 17:41:19 +0530

    First Project

---
+++ src/org/bank/AxisBank.java
@@ -0,0 +1,14 @@
+package org.bank;
+
+public class AxisBank extends IbBank {
+
+@Override
+public void saving() {
+	// TODO Auto-generated method stub
+	super.saving();
+}
+	
+
+	
+
+}
---
+++ src/org/bank/IbBank.java
@@ -0,0 +1,13 @@
+package org.bank;
+
+public class IbBank {
+public void saving() {
+	System.out.println("Ibbank savings is : 8%");
+
+}public void fixed() {
+System.out.println("Ibbank Fixed is :9%");
+
+}public void deposit() {
+	System.out.println("Ibbank deposit is:6%");
+}
+}
